b2be3261e487b1050e640e0bf68a702a00e5a381
Luke Solomon <luke@yidio.com>
2017-04-11 23:44:44 -0700

    Fixes for a new version of Charts iOS

--- HackerRankMobile/DetailViewController.swift
+++ HackerRankMobile/DetailViewController.swift
@@ -21,7 +21,7 @@ class DetailViewController: UIViewController {
     }
 
     
-
+    
     /*
     // MARK: - Navigation
 
--- HackerRankMobile/MainViewController.swift
+++ HackerRankMobile/MainViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 
-
 class MainViewController: UIViewController {
     
     // Mark: - Variables
